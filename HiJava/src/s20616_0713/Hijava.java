69f35da05dc16987bbbdd1aaefa4f2c7f3fb8e3f
Home <Home@192.168.200.179>
2021-07-13 14:30:44 +0900

    첫번째 소스

---
+++ HiJava/src/s20616_0713/Hijava.java
@@ -0,0 +1,12 @@
+package s20616_0713;
+
+public class Hijava {
+	
+	public static void main(String[] args) {
+		System.out.println("첫번째 깃 소스");
+		
+		System.out.println("안녕, 소스 추가합니다.");
+		
+	}
+}
+	
